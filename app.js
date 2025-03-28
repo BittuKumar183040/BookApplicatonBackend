39f7ea40fd5e27893d8c48fe879577da6587a7b5
Bittu Kumar <64475463+BittuKumar183040@users.noreply.github.com>
2025-03-29 00:51:10 +0530

    Rewamp Files

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const cors = require('cors');
 
-let indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-let app = express();
+const app = express();
 const corsOptions = {
   origin: '*',
   methods: 'GET, POST',
   allowedHeaders: ['Content-Type', 'user']
 };
 app.use(cors(corsOptions));
-
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-app.use(function (req, res, next) {
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.use(function (err, req, res, next) {
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
